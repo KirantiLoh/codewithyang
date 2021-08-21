525e13e91e30e470021f2e1886961d99fc33bd7c
Maurice Yang <mauriceyangmy28@gmail.com>
2021-08-21 20:30:20 +0700

    Setting env vars

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-iy8!u747-umvokdv6i6fhp-f81(=e*-tabk1v2p7&o2vfri7+l'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["codewithyang.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = "darkprismzmy05@gmail.com"
-EMAIL_HOST_PASSWORD = "Darkness28012005"
+EMAIL_HOST_USER = os.getenv("email_host")
+EMAIL_HOST_PASSWORD = os.getenv("email_pass")
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
