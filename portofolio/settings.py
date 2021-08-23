4655e998cd687578960b8e5df419c353b174553e
Maurice Yang <mauriceyangmy28@gmail.com>
2021-08-23 09:27:35 +0700

    Edited settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -132,7 +132,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 
 MEDIA_URL = "/media/"
 
