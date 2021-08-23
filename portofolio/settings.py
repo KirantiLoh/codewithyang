9a1e91da34f3f1dfc7b8bc104aab4b9365d4a200
Maurice Yang <mauriceyangmy28@gmail.com>
2021-08-23 09:21:24 +0700

    Edited settings.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG"))
 
-ALLOWED_HOSTS = ["codewithyang.herokuapp.com"]
+ALLOWED_HOSTS = ["codewithyang.herokuapp.com", "*"]
 
 
 # Application definition
@@ -152,3 +152,36 @@ EMAIL_HOST_PASSWORD = os.getenv("email_pass")
 EMAIL_USE_TLS = True
 
 django_heroku.settings(locals())
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'mysite.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'MYAPP': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
