a62ce789c2decb1d26cd82c7fbdcc39ccb814634
Maurice Yang <mauriceyangmy28@gmail.com>
2021-08-23 11:13:04 +0700

    Edited views.py

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["codewithyang.herokuapp.com"]
+ALLOWED_HOSTS = ["codewithyang.herokuapp.com/"]
 
 
 # Application definition
