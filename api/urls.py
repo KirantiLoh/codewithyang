55fd9625335a161c89f96bc950340741bccc3735
Maurice Yang <mauriceyangmy28@gmail.com>
2021-08-27 22:30:16 +0700

    Edited urls.py

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,4 @@ urlpatterns = [
     path('project/<int:id>', views.project_detail_view, name="Project Detail"),
     path("add_project", views.add_project_view, name="Add Project"),
     path('error', views.error_page, name="Error"),
-    path("about/<str:random_string>", views.error_redirect, name="About Error Redirect"),
-    path("contact/<str:random_string>", views.error_redirect, name="Contact Error Redirect"),
-    path("<str:random_string>", views.error_redirect, name = "Error Redirect"),
-    
 ]
